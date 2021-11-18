93e002d26a91ff92be1668ff2aef1dc4a52b3db6
Akuma <jeanothniel36@gmail.com>
2021-11-19 00:12:50 +0100

    Postcard update2

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import moment from 'moment';
+import Image from 'next/image';
 import Link from 'next/link';
+import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => {
     
@@ -20,8 +22,10 @@ const PostCard = ({ post }) => {
             </h1>
             <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
                 <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-                    <img
+                    <Image
+                        unoptimized
                         alt={post.author.nom}
+                        loader={grpahCMSImageLoader}
                         height="30px"
                         width="30px"
                         className="align-middle rounded-full"
