8f582a631e1af3815716840122f2ad74053fcb08
Akuma <jeanothniel36@gmail.com>
2022-03-30 06:29:14 +0200

    header reverse

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect} from 'react'
 import Link from 'next/link';
 import { getCategories } from '../services';
+import Image from 'next/image';
+import logo from '../public/logo.png'
+
 
 
 const Header = () => {
@@ -12,16 +15,15 @@ const Header = () => {
     }, []);
 
 
-
-
     return (
         <div className="container mx-auto px-10 mb-8">
             <div className="border-b w-full inline-block border-blue-400 py-8">
                 <div className="md:float-left block">
                     <Link href ="/">
-                        <span className="cursor-pointer font-bold text-4xl text-white">
+                        <Image src={logo} alt="logo" height="45px" />
+                        {/* <span className="cursor-pointer font-bold text-4xl text-white">
                             Blog
-                        </span>
+                        </span> */}
                     </Link>
                 </div>
                 <div className="hidden md:float-left md:contents">
